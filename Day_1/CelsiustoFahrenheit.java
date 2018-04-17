5646a07aa1997f4571348d2ce3bb72624806c365
bhanuchander <bhanuchanderu@nmsworks.co.in>
2018-04-17 15:25:04 +0530

    java exercises added ...

---
+++ Day_1/CelsiustoFahrenheit.java
@@ -0,0 +1,14 @@
+public class CelsiustoFahrenheit {
+
+        public static double celsiusToFahrenheit (double celsius)
+        {
+            double fahrenheit = ((9.0/5)*celsius)+32;
+            return fahrenheit;
+        }
+
+        public static double fahrenheitToCelsius (double fahrenheit)
+        {
+            double celsius = (fahrenheit-32)*(5.0/9);
+            return celsius;
+        }
+}
